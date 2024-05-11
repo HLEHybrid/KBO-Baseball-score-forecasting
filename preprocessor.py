2235d9ed5479c70e38f7e01736a3fd579572ea44
HLEHybrid <132531216+HLEHybrid@users.noreply.github.com>
2024-05-12 07:52:11 +0900

    test 백업

---
+++ preprocessor.py
@@ -0,0 +1,355 @@
+from requests.compat import *
+from requests import request
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+import numpy as np
+from scraper import recode_scraper, gamelog_scraper
+
+def bat_recode(year):
+    batting_bat = recode_scraper('batting','bat',year)
+    batting_bat['Hitter Number'] = batting_bat['Number']
+    batting_bat['타자 이름'] = batting_bat['이름']
+    batting_bat = batting_bat[['Hitter Number','타자 이름','Batting Average on Balls In Play, 인플레이 타구의 안타 비율','땅볼%','내야 뜬볼%',
+                                     '외야 뜬볼%','뜬볼%','라인드라이브%','홈런 / 뜬볼%','내야안타%']]
+    
+    batting_direction = recode_scraper('batting','direction',year)
+    batting_direction['Hitter Number'] = batting_direction['Number']
+    batting_direction['타자 이름'] = batting_direction['이름']
+    batting_direction = batting_direction[['Hitter Number','타자 이름','전체 좌측 타구 비율','전체 좌중앙 타구 비율','전체 중앙 타구 비율',
+                                               '전체 우중앙 타구 비율','전체 우측 타구 비율']]
+    
+    batting_pitch = recode_scraper('batting','pitch',year)
+    batting_pitch['Hitter Number'] = batting_pitch['Number']
+    batting_pitch['타자 이름'] = batting_pitch['이름']
+    batting_pitch = batting_pitch[['Hitter Number','타자 이름','Strike%, 전체 투구 대비 스트라이크', 'Called Strike%, 전체 투구 대비 루킹 스트라이크%',
+       'Whiff%, 전체 투구 대비 헛스윙 스트라이크%', 'CSW%, 전체 투구 대비 루킹+헛스윙 스트라이크%',
+       'Swing%, 스윙 비율', '스윙 대비 콘택트 비율', '스윙 대비 헛스윙 비율', '초구 스트라이크 비율',
+       '초구 스윙 비율', '투스트라이크 카운트 투구 대비 삼진 결정 비율', 'Strike Zone%, 존 안에 들어온 투구 비율',
+       'Strike Zone Swing%, 존 안에 들어온 투구 대비 스윙 비율',
+       'Strike Zone Contact%, 존 안에 들어온 투구 대비 콘택트 비율',
+       'Out Zone%, 존 밖에 들어온 투구 비율', 'Out Zone Swing%, 존 밖에 들어온 투구 대비 스윙 비율',
+       'Out Zone Contact%, 존 밖에 들어온 투구 대비 콘택트 비율',
+       'Meatball Zone%, 존 한가운데 들어온 투구 비율',
+       'Meatball Swing%, 존 한가운데 들어온 투구 대비 스윙 비율',
+       'Shadow Zone%, 쉐도우존에 들어온 투구 비율', 'Looking Strike Out%, 루킹 삼진 비율']]
+    
+    batting_balltype = recode_scraper('batting','ballType',year)
+    batting_balltype = balltype_value_divider(batting_balltype, 'batting')
+
+    batting_player_data = pd.merge(batting_bat,batting_direction)
+    batting_player_data = pd.merge(batting_pitch,batting_player_data)
+    batting_player_data = pd.merge(batting_balltype,batting_player_data)
+
+    batting_split_data = []
+
+    for id in list(batting_player_data['Hitter Number']):
+        batting_split_num = split_finder(id,'batting')
+        batting_split_data.append(batting_split_num)
+
+    batting_player_data['투타'] = batting_split_data
+
+    return batting_player_data
+
+
+def pitch_recode(year):
+    pitching_balltype = recode_scraper('pitching','ballType',year)
+    pitching_balltype = balltype_value_divider(pitching_balltype, 'pitching')
+    
+    pitching_pitch = recode_scraper('pitching','pitch',year)
+    pitching_pitch.columns = ['(P) ' + column for column in pitching_pitch.columns]
+    pitching_pitch['Pitcher Number'] = pitching_pitch['(P) Number']
+    pitching_pitch['투수 이름'] = pitching_pitch['(P) 이름']
+    pitching_pitch = pitching_pitch[['Pitcher Number','투수 이름', '(P) Strike%, 전체 투구 대비 스트라이크', '(P) Called Strike%, 전체 투구 대비 루킹 스트라이크%',
+       '(P) Whiff%, 전체 투구 대비 헛스윙 스트라이크%', '(P) CSW%, 전체 투구 대비 루킹+헛스윙 스트라이크%',
+       '(P) Swing%, 스윙 비율', '(P) 스윙 대비 콘택트 비율', '(P) 스윙 대비 헛스윙 비율',
+       '(P) 초구 스트라이크 비율', '(P) 초구 스윙 비율', '(P) 투스트라이크 카운트 투구 대비 삼진 결정 비율',
+       '(P) Strike Zone%, 존 안에 들어온 투구 비율',
+       '(P) Strike Zone Swing%, 존 안에 들어온 투구 대비 스윙 비율',
+       '(P) Strike Zone Contact%, 존 안에 들어온 투구 대비 콘택트 비율',
+       '(P) Out Zone%, 존 밖에 들어온 투구 비율',
+       '(P) Out Zone Swing%, 존 밖에 들어온 투구 대비 스윙 비율',
+       '(P) Out Zone Contact%, 존 밖에 들어온 투구 대비 콘택트 비율',
+       '(P) Meatball Zone%, 존 한가운데 들어온 투구 비율',
+       '(P) Meatball Swing%, 존 한가운데 들어온 투구 대비 스윙 비율','(P) Looking Strike Out%, 루킹 삼진 비율']]
+    
+    pitching_direction = recode_scraper('pitching','direction',year)
+    pitching_direction.columns = ['(P) ' + column for column in pitching_direction.columns]
+    pitching_direction['Pitcher Number'] = pitching_direction['(P) Number']
+    pitching_direction['투수 이름'] = pitching_direction['(P) 이름']
+    pitching_direction = pitching_direction[['Pitcher Number','투수 이름', '(P) Left ball%, 전체 좌측 타구 비율',
+       '(P) Left center ball%, 전체 좌중앙 타구 비율', '(P) Center ball%, 전체 중앙 타구 비율',
+       '(P) Right center ball%, 전체 우중앙 타구 비율', '(P) Right ball%, 전체 우측 타구 비율',
+       '(P) Pull-side ball%, 전체 당겨친 타구 비율']]
+    
+    pitching_player_data = pd.merge(pitching_balltype,pitching_pitch)
+    pitching_player_data = pd.merge(pitching_direction,pitching_player_data)
+
+    pitching_split_data = []
+
+    for id in list(pitching_player_data['Pitcher Number']):
+        pitching_split_num = split_finder(id,'pitching')
+        pitching_split_data.append(pitching_split_num)
+
+    pitching_player_data['(P) 투타'] = pitching_split_data
+    
+    return pitching_player_data
+
+
+
+def balltype_value_divider(data, type):
+    
+    if type == 'batting':
+        data['Hitter Number'] = data['Number']
+        data['타자 이름'] = data['이름']
+
+        balltype_list = ['구종가치 (투심)',
+               '구종가치 (포심)', '구종가치 (커터)', '구종가치 (커브)', '구종가치 (슬라이더)', '구종가치 (체인지업)',
+               '구종가치 (싱커)', '구종가치 (포크볼)']
+        balltype_throw_list = ['구종별 투구 수 (투심)',
+           '구종별 투구 수 (포심)', '구종별 투구 수 (커터)', '구종별 투구 수 (커브)', '구종별 투구 수 (슬라이더)',
+           '구종별 투구 수 (체인지업)', '구종별 투구 수 (싱커)', '구종별 투구 수 (포크볼)']
+        balltype_100_list = ['구종가치/100 (투심)',
+           '구종가치/100 (포심)', '구종가치/100 (커터)', '구종가치/100 (커브)', '구종가치/100 (슬라이더)',
+           '구종가치/100 (체인지업)', '구종가치/100 (싱커)', '구종가치/100 (포크볼)']
+    elif type == 'pitching':
+        data['Pitcher Number'] = data['Number']
+        data['투수 이름'] = data['이름']
+        
+        balltype_list = ['2-Seamer Fastball Pitch Value, 구종가치 (투심)',
+       '4-Seamer Fastball Pitch Value, 구종가치 (포심)',
+       'Cutter Pitch Value, 구종가치 (커터)', 'Curve Pitch Value, 구종가치 (커브)',
+       'Slider Pitch Value, 구종가치 (슬라이더)', 'Changeup Pitch Value, 구종가치 (체인지업)',
+       'Sinker Pitch Value, 구종가치 (싱커)', 'Forkball Pitch Value, 구종가치 (포크볼)']
+        balltype_throw_list = ['2-Seamer Fastball Pitch Count, 구종별 투구 수 (투심)',
+       '4-Seamer Fastball Pitch Count, 구종별 투구 수 (포심)',
+       'Cutter Pitch Count, 구종별 투구 수 (커터)', 'Curve Pitch Count, 구종별 투구 수 (커브)',
+       'Slider Pitch Count, 구종별 투구 수 (슬라이더)',
+       'Changeup Pitch Count, 구종별 투구 수 (체인지업)',
+       'Sinker Pitch Count, 구종별 투구 수 (싱커)',
+       'Forkball Pitch Count, 구종별 투구 수 (포크볼)']
+        balltype_speed_list = ['2-Seamer Fastball Velocity, 평균구속 (투심)','4-Seamer Fastball Velocity, 평균구속 (포심)',
+                              'Cutter Velocity, 평균구속 (커터)', 'Curve Velocity, 평균구속 (커브)',
+                               'Slider Velocity, 평균구속 (슬라이더)','Changeup Velocity, 평균구속 (체인지업)',
+                              'Sinker Velocity, 평균구속 (싱커)','Forkball Velocity, 평균구속 (포크볼)']
+        balltype_100_list = ['2-Seamer Fastball Pitch Value per 100, 구종가치/100 (투심)',
+       '4-Seamer Fastball Pitch Value per 100, 구종가치/100 (포심)',
+       'Cutter Pitch Value per 100, 구종가치/100 (커터)',
+       'Curve Pitch Value per 100, 구종가치/100 (커브)',
+       'Slider Pitch Value per 100, 구종가치/100 (슬라이더)',
+       'Changeup Pitch Value per 100, 구종가치/100 (체인지업)',
+       'Sinker Pitch Value per 100, 구종가치/100 (싱커)',
+       'Forkball Pitch Value per 100, 구종가치/100 (포크볼)']
+        
+    for balltype, balltype_throw, balltype_100 in zip(balltype_list, balltype_throw_list, balltype_100_list):
+        balltype_value_list = [float(value) if value != '' else np.nan for value in data[balltype]]
+        balltype_throw_num_list = [float(value) if value != '' else np.nan for value in data[balltype_throw]]
+        balltype_throw_num_list = np.array(balltype_throw_num_list)
+        balltype_throw_num_list[balltype_throw_num_list < 10] = np.nan
+        
+        data[balltype_100] = balltype_value_list / balltype_throw_num_list * 100
+    
+    if type == 'batting':
+        data = data[['Hitter Number','타자 이름'] + balltype_100_list]
+    elif type == 'pitching':
+        for balltype_speed in balltype_speed_list:
+            data[balltype_speed] = [float(value) if value != '' else np.nan for value in data[balltype_speed]]
+        
+        data = data[['Pitcher Number','투수 이름'] + balltype_speed_list + balltype_100_list]
+
+            # 열의 데이터를 숫자로 변환
+    for col in data.columns[3:]:
+        data[col] = pd.to_numeric(data[col], errors='coerce')
+    
+    # 결측치 처리
+    for col in range(data.shape[1]-2):
+        column_data = data.iloc[:, col+2]
+        valid_data = column_data[~pd.isna(column_data)]
+        Q1 = np.nanquantile(valid_data, 0.25)
+        Q3 = np.nanquantile(valid_data, 0.75)
+        IQR = Q3 - Q1
+
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+
+        # 이상치를 제외한 최솟값과 최댓값 계산
+        filtered_data = valid_data[(valid_data >= lower_bound) & (valid_data <= upper_bound)]
+        filtered_min = np.min(filtered_data)
+
+        # 결측치 대체 전략 설정
+        replacement_value = filtered_min if filtered_min < Q3 else Q3
+
+        # 결측치 대체
+        data.iloc[pd.isna(data.iloc[:, col+2]), col+2] = replacement_value
+    
+    return data
+
+
+def split_finder(player_num,type):
+    url = 'http://statiz.sporki.com/player'
+
+    params = {
+        'm':'playerinfo',
+        'p_no':player_num
+    }
+
+    resp = request('GET',url=url, params=params)
+    dom = BeautifulSoup(resp.text,'html.parser')
+    
+    print(player_num)
+    
+    text = dom.find_all(attrs={'class':'con'})[0].text[-5:-1]
+    
+    if type == 'pitching':
+        pitching_type = text[0:2]
+        
+        if pitching_type == '우투':
+            return 0.0
+        
+        elif pitching_type == '좌투':
+            return 1.0
+        
+        elif pitching_type == '우언':
+            return 0.25
+        
+        elif pitching_type == '좌언':
+            return 0.75
+        
+    elif type == 'batting':
+        batting_type = text[2:4]
+        
+        if batting_type == '우타':
+            return 0.0
+        
+        elif batting_type == '좌타':
+            return 1.0
+        
+        elif batting_type == '양타':
+            return 0.5
+        
+
+def gamelog_agg(year):
+    gamelog = gamelog_scraper(year)
+    batting_player_data = bat_recode(year)
+    pitching_player_data = pitch_recode(year)
+
+    # 2024년 4월 24일 현재 전체 시즌 파크 팩터
+    park_factors = {'잠실':895, '사직':1064,'창원':1051,'대구':1111,'수원':1020,'문학':985, '고척':965, '대전':1019, '광주':1000,
+                '울산':1000,'포항':1000}
+    park_factors_list = []
+    for stadium in list(gamelog['경기장']):
+        park_factors_list.append(park_factors[stadium])
+        
+    gamelog['파크팩터'] = park_factors_list
+
+    gamelog_agg = pd.merge(gamelog, batting_player_data)
+    gamelog_agg = pd.merge(gamelog_agg,pitching_player_data)
+
+    result_list = []
+
+    for result in list(gamelog_agg['결과']):
+        if '플라이 아웃' in result:
+            result_list.append('아웃')
+        elif '땅볼 아웃' in result:
+            result_list.append('아웃')
+        elif '병살타' in result:
+            result_list.append('더블플레이')
+        elif '실책' in result:
+            result_list.append('1루타')
+        elif '1루타' in result:
+            result_list.append('1루타')
+        elif '2루타' in result:
+            result_list.append('2루타')
+        elif '3루타' in result:
+            result_list.append('3루타')
+        elif '내야 안타' in result:
+            result_list.append('1루타')
+        elif '홈런' in result:
+            result_list.append('홈런')
+        elif '직선타 아웃' in result:
+            result_list.append('아웃')
+        elif '삼진' in result:
+            result_list.append('아웃')
+        elif '야수선택' in result:
+            result_list.append('아웃')
+        elif '땅볼 출루' in result:
+            result_list.append('아웃')
+        elif '사구' in result:
+            result_list.append('사사구')
+        elif '3번트 아웃' in result:
+            result_list.append('아웃')
+        elif '4구' in result:
+            result_list.append('사사구')
+        elif '선행주자아웃 출루' in result:
+            result_list.append('아웃')
+        else:
+            result_list.append(None)
+
+    gamelog_agg['Result'] = result_list
+
+    gamelog_agg = gamelog_agg[['날짜','Pitcher Number', 'Hitter Number','투수 이름','타자 이름','내야 수비 RAA',
+       '외야 수비 RAA', '포수 수비 RAA', '구종가치/100 (투심)',
+       '구종가치/100 (포심)', '구종가치/100 (커터)', '구종가치/100 (커브)', '구종가치/100 (슬라이더)',
+       '구종가치/100 (체인지업)', '구종가치/100 (싱커)', '구종가치/100 (포크볼)',
+       'Strike%, 전체 투구 대비 스트라이크', 'Called Strike%, 전체 투구 대비 루킹 스트라이크%',
+       'Whiff%, 전체 투구 대비 헛스윙 스트라이크%', 'CSW%, 전체 투구 대비 루킹+헛스윙 스트라이크%',
+       'Swing%, 스윙 비율', '스윙 대비 콘택트 비율', '스윙 대비 헛스윙 비율', '초구 스트라이크 비율',
+       '초구 스윙 비율', '투스트라이크 카운트 투구 대비 삼진 결정 비율', 'Strike Zone%, 존 안에 들어온 투구 비율',
+       'Strike Zone Swing%, 존 안에 들어온 투구 대비 스윙 비율',
+       'Strike Zone Contact%, 존 안에 들어온 투구 대비 콘택트 비율',
+       'Out Zone%, 존 밖에 들어온 투구 비율', 'Out Zone Swing%, 존 밖에 들어온 투구 대비 스윙 비율',
+       'Out Zone Contact%, 존 밖에 들어온 투구 대비 콘택트 비율',
+       'Meatball Zone%, 존 한가운데 들어온 투구 비율',
+       'Meatball Swing%, 존 한가운데 들어온 투구 대비 스윙 비율',
+       'Shadow Zone%, 쉐도우존에 들어온 투구 비율', 'Looking Strike Out%, 루킹 삼진 비율',
+       'Batting Average on Balls In Play, 인플레이 타구의 안타 비율', '땅볼%', '내야 뜬볼%',
+       '외야 뜬볼%', '뜬볼%', '라인드라이브%', '홈런 / 뜬볼%', '내야안타%', '전체 좌측 타구 비율',
+       '전체 좌중앙 타구 비율', '전체 중앙 타구 비율', '전체 우중앙 타구 비율', '전체 우측 타구 비율', '투타',
+       '(P) Left ball%, 전체 좌측 타구 비율', '(P) Left center ball%, 전체 좌중앙 타구 비율',
+       '(P) Center ball%, 전체 중앙 타구 비율', '(P) Right center ball%, 전체 우중앙 타구 비율',
+       '(P) Right ball%, 전체 우측 타구 비율', '(P) Pull-side ball%, 전체 당겨친 타구 비율',
+        '2-Seamer Fastball Velocity, 평균구속 (투심)',
+       '4-Seamer Fastball Velocity, 평균구속 (포심)', 'Cutter Velocity, 평균구속 (커터)',
+       'Curve Velocity, 평균구속 (커브)', 'Slider Velocity, 평균구속 (슬라이더)',
+       'Changeup Velocity, 평균구속 (체인지업)', 'Sinker Velocity, 평균구속 (싱커)',
+       'Forkball Velocity, 평균구속 (포크볼)',
+       '2-Seamer Fastball Pitch Value per 100, 구종가치/100 (투심)',
+       '4-Seamer Fastball Pitch Value per 100, 구종가치/100 (포심)',
+       'Cutter Pitch Value per 100, 구종가치/100 (커터)',
+       'Curve Pitch Value per 100, 구종가치/100 (커브)',
+       'Slider Pitch Value per 100, 구종가치/100 (슬라이더)',
+       'Changeup Pitch Value per 100, 구종가치/100 (체인지업)',
+       'Sinker Pitch Value per 100, 구종가치/100 (싱커)',
+       'Forkball Pitch Value per 100, 구종가치/100 (포크볼)',
+       '(P) Strike%, 전체 투구 대비 스트라이크', '(P) Called Strike%, 전체 투구 대비 루킹 스트라이크%',
+       '(P) Whiff%, 전체 투구 대비 헛스윙 스트라이크%', '(P) CSW%, 전체 투구 대비 루킹+헛스윙 스트라이크%',
+       '(P) Swing%, 스윙 비율', '(P) 스윙 대비 콘택트 비율', '(P) 스윙 대비 헛스윙 비율',
+       '(P) 초구 스트라이크 비율', '(P) 초구 스윙 비율', '(P) 투스트라이크 카운트 투구 대비 삼진 결정 비율',
+       '(P) Strike Zone%, 존 안에 들어온 투구 비율',
+       '(P) Strike Zone Swing%, 존 안에 들어온 투구 대비 스윙 비율',
+       '(P) Strike Zone Contact%, 존 안에 들어온 투구 대비 콘택트 비율',
+       '(P) Out Zone%, 존 밖에 들어온 투구 비율',
+       '(P) Out Zone Swing%, 존 밖에 들어온 투구 대비 스윙 비율',
+       '(P) Out Zone Contact%, 존 밖에 들어온 투구 대비 콘택트 비율',
+       '(P) Meatball Zone%, 존 한가운데 들어온 투구 비율',
+       '(P) Meatball Swing%, 존 한가운데 들어온 투구 대비 스윙 비율',
+       '(P) Looking Strike Out%, 루킹 삼진 비율', '(P) 투타', '파크팩터', 'Result']]
+    
+    # 1루타(1베이스 출루), 2루타(2베이스 출루), 3루타(3베이스 출루), 홈런, 아웃(삼진 + 플라이 아웃), 더블플레이, 사사구 외 상황 제거
+    gamelog_agg = gamelog_agg.dropna(subset=['Result'])
+
+    gamelog_agg.to_excel('gamelog_agg_' + str(year) + '.xlsx')
+
+    return gamelog_agg
+
+
+if __name__ == "__main__":
+    # 분석을 원하는 연도 입력
+    years = [2022,2023]
+    gamelog_all = pd.DataFrame()
+
+    for year in years:
+        gamelog = gamelog_agg(year)
+        gamelog_all = pd.concat([gamelog_all,gamelog],axis=0)
+    
+    gamelog_all.to_excel('gamelog_agg.xlsx')
---
+++ scraper.py
@@ -0,0 +1,266 @@
+from requests.compat import *
+from requests import request
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import numpy as np
+
+def recode_scraper(category1, category2, year):
+    url = 'http://statiz.sporki.com/stats'
+        
+    params = {
+        'm':'main',
+        'm2':category1,
+        'm3':category2,
+        'year':str(year),
+        'pr':'1000',
+        'reg':'C30',
+        'ls':'1'
+    }
+
+    resp = request('GET',url=url, params=params)
+    dom = BeautifulSoup(resp.text,'html.parser')
+    
+    data = [node.text for node in dom.find_all('td')]
+    position_data = dom.select('div > span')
+    
+    players = [item.text for item in dom.select('td > div > a')]
+    players_href = [tag.attrs['href'] for tag in dom.select('td> div > a')]
+    players_num = [re.search(r'\d+', item).group() for item in players_href]
+    attrs_2 = [item['title'] for item in dom.select('li[title]')]
+    if (category1 == 'pitching' and category2 == 'pitch'):
+        attrs_2[19] = 'Looking Strike Out, 루킹 삼진'
+        attrs_2[20] = 'Swing Strike Out, 헛스윙 삼진'
+    attrs = attrs_2[:int(len(attrs_2)/2)]
+    columns = ['Number', '이름','Position'] + attrs
+    df = pd.DataFrame(columns = columns)
+
+    for i in range(len(players)):
+        row = {}
+        row['Number'] = players_num[i]
+        row['이름'] = players[i]
+        if (category1 == 'fielding'):
+            row['Position'] = position_data[30+3*i].text
+        else:
+            row['Position'] = position_data[48+3*i].text
+        for j in range(len(attrs)):
+            index = i*(len(attrs)+4) + 4 + j
+            row[attrs[j]] = data[index]
+
+        df = pd.concat([df, pd.DataFrame([row])], ignore_index=True)
+    
+    return df
+
+def get_position_code(position):
+    """각 포지션에 대한 코드를 반환합니다."""
+    if position == '1루수':
+        return '1B'
+    elif position == '2루수':
+        return '2B'
+    elif position == '3루수':
+        return '3B'
+    elif position == '유격수':
+        return 'SS'
+    elif position == '좌익수':
+        return 'LF'
+    elif position == '중견수':
+        return 'CF'
+    elif position == '우익수':
+        return 'RF'
+    elif position == '포수':
+        return 'C'
+    else:
+        return None
+
+def get_raa_sum(records_df, player_number, pos_code):
+    try:
+        specific_record = records_df[(records_df['Number'] == player_number) & (records_df['Position'] == pos_code)]
+        raa = float(specific_record['Fielding Runs Above Average/144 Games, 평균 대비 수비 득점 기여 (144경기 기준)'].iloc[0])
+    except (ValueError, IndexError):
+        raa_list = list(map(float, records_df[records_df['Number'] == player_number]['Fielding Runs Above Average/144 Games, 평균 대비 수비 득점 기여 (144경기 기준)']))
+        raa = sum(raa_list) / len(raa_list) if raa_list else 0
+    return raa
+
+def calculate_raa_sums(positions_num, outfield_recode, infield_recode, catcher_recode):
+    outfield_raa_sum = infield_raa_sum = catcher_raa_sum = 0.0
+    position_keys = list(positions_num.keys())
+
+    for i, player_number in enumerate(positions_num.values()):
+        position = position_keys[i]
+#         print(position)
+        pos_code = get_position_code(position)
+#         print(pos_code)
+#         print(player_number)
+        
+        if position in ['우익수', '좌익수', '중견수']:
+            outfield_raa_sum += get_raa_sum(outfield_recode, player_number, pos_code)
+        elif position in ['1루수', '2루수', '유격수', '3루수']:
+            infield_raa_sum += get_raa_sum(infield_recode, player_number, pos_code)
+        elif position in ['포수']:
+            players_recode = catcher_recode[catcher_recode['Number'] == player_number]
+            try:
+                raa_include_framing = float(players_recode[players_recode['Position'] == 'C']['Fielding Runs Above Average/144 Games, 평균 대비 수비 득점 기여 (144경기 기준)'])
+                raa_framing_144 = (float(players_recode[players_recode['Position'] == 'C']['Fielding Runs (Framing), 프레이밍 관련 득점 기여'])*144) / float(players_recode[players_recode['Position'] == 'C']['Games Played, 출장'])
+                raa = raa_include_framing - raa_framing_144
+                catcher_raa_sum = catcher_raa_sum + raa
+            except:
+                raa_list = list(map(float,list(players_recode['Fielding Runs Above Average/144 Games, 평균 대비 수비 득점 기여 (144경기 기준)'])))
+                raa = sum(raa_list)/len(raa_list) if raa_list else 0
+                catcher_raa_sum = catcher_raa_sum + raa
+
+    return outfield_raa_sum, infield_raa_sum, catcher_raa_sum
+
+def gamelog_scraper(year):
+    
+    games_schedule = list()
+    
+    # 1 ~ 12월까지 경기 ID 수집
+    for i in range(1,13):
+        url = 'http://statiz.sporki.com/schedule'
+    
+        params = {
+            'year':str(year),
+            'month':str(i)
+        }
+        
+        resp = request('GET',url=url, params=params)
+        dom = BeautifulSoup(resp.text,'html.parser')
+        
+        if (['p_schedule'] in list(dom.select('div > ul > li >a')[-1].attrs.values())):
+            print('skip')
+            continue
+        
+        print(i)
+        games_tag = [tag for tag in dom.select('div > ul > li >a') if 'class' not in list(tag.attrs.keys())]
+        games_href = [tag.attrs['href'] for tag in games_tag]
+        games_num = [re.search(r'\d+', item).group() for item in games_href]
+        
+        games_schedule = games_schedule + games_num
+    
+    # Gamelog feature 지정
+    columns = ['날짜', '경기장', 'Pitcher Number','Hitter Number','회', '내야 수비 RAA','외야 수비 RAA','포수 수비 RAA', '투수 이름', 
+                '타자 이름', '투구수', '결과', '이전상황','이후상황','LEV','REs','REa','WPe','WPa']
+    df = pd.DataFrame(columns = columns)
+    
+    # 수비 데이터 호출
+    outfield_recode = recode_scraper('fielding','outField',year)
+    infield_recode = recode_scraper('fielding','inField',year)
+    catcher_recode = recode_scraper('fielding','catcher',year)
+    
+    # 게임 별 Gamelog 수집
+    for game_id in games_schedule:
+        
+        url = 'http://statiz.sporki.com/schedule'
+        
+        params = {
+            'm':'gamelogs',
+            's_no':game_id
+        }
+
+        resp = request('GET',url=url, params=params)
+        dom = BeautifulSoup(resp.text,'html.parser')
+        
+        stadium = dom.find_all(attrs={'class':'txt'})[1].text[7:9]
+        date = dom.find_all(attrs={'class':'txt'})[1].text[11:16]
+        print(stadium, date)
+        game_canceled = dom.find_all(attrs={'style':'font-size:1.6rem;'})
+    
+        if (len(game_canceled) > 0):
+            continue
+        
+        data = [node.text for node in dom.find_all('td')]
+        lineup_data = data[:80]
+        data = data[80:-4]
+        
+        players = [item.text for item in dom.select('td > a')]
+        players_href = [tag.attrs['href'] for tag in dom.select('td > a')]
+        players_num = []
+
+        for item in players_href:
+            match = re.search(r'\d+', item)
+            if match:
+                players_num.append(match.group())
+            else:
+                players_num.append('')
+                
+        players_num_lineup = players_num[:20]
+        lineup_columns = ['Player Number', 'Name', 'Position', 'Bats/Throws', 'Home/Away']
+        lineup_df = pd.DataFrame(columns=lineup_columns)
+
+        home_away_toggle = 'Away'  # 초기 팀을 'Away'로 설정
+        last_number = None
+
+        for i in range(0, len(lineup_data), 4):
+            number = lineup_data[i]
+            name = lineup_data[i + 1]
+            position = lineup_data[i + 2]
+            bats_throws = lineup_data[i + 3]
+
+            # 번호가 '1'로 재시작하면 Home/Away 토글
+            if number == '1' and last_number is not None:
+                home_away_toggle = 'Away' if home_away_toggle == 'Home' else 'Home'
+
+            player_data = pd.DataFrame({
+                'Name': [name],
+                'Position': [position],
+                'Bats/Throws': [bats_throws],
+                'Home/Away': [home_away_toggle]
+            })
+            lineup_df = pd.concat([lineup_df, player_data], ignore_index=True)
+            last_number = number
+
+        for i in range(len(players_num_lineup)):
+            number = players_num_lineup[i]
+            lineup_df['Player Number'][i] = number
+            
+        # Starting lineup에서 Position 연결
+        positions = ['좌익수', '중견수', '우익수', '1루수', '2루수', '유격수', '3루수','포수']
+        positions_num_home = {}
+        positions_num_away = {}
+
+        df_positions = lineup_df[(lineup_df['Position'].isin(positions))]
+        i = 0
+        j = 0
+
+        for home_player_position in list(df_positions[df_positions['Home/Away'] == 'Home']['Position']):
+            positions_num_home[home_player_position] = list(df_positions[df_positions['Home/Away'] == 'Home']['Player Number'])[i]
+            i = i+1
+        for away_player_position in list(df_positions[df_positions['Home/Away'] == 'Away']['Position']):
+            positions_num_away[away_player_position] = list(df_positions[df_positions['Home/Away'] == 'Away']['Player Number'])[j]
+            j = j+1
+        
+        # Home/Away 수비 RAA 산출
+        home_outfield_raa_sum, home_infield_raa_sum, home_catcher_raa_sum = calculate_raa_sums(positions_num_home, outfield_recode, infield_recode, catcher_recode)
+        away_outfield_raa_sum, away_infield_raa_sum, away_catcher_raa_sum = calculate_raa_sums(positions_num_away, outfield_recode, infield_recode, catcher_recode)
+        
+        # Gamelog 생성
+        for i in range(int(len(data)/12)):
+            row = {}
+            
+            row['날짜'] = date
+            row['경기장'] = stadium
+            row['Pitcher Number'] = players_num[2*i+20]
+            row['Hitter Number'] = players_num[2*i + 21]
+            if data[12*i] != '':
+                row['회'] = data[12*i]
+                inning = data[12*i]
+            else:
+                row['회'] = inning
+                
+            # 수비 RAA 지정
+            if row['회'][-1] == '초':
+                row['내야 수비 RAA'] = home_infield_raa_sum
+                row['외야 수비 RAA'] = home_outfield_raa_sum
+                row['포수 수비 RAA'] = home_catcher_raa_sum
+            else:
+                row['내야 수비 RAA'] = away_infield_raa_sum
+                row['외야 수비 RAA'] = away_outfield_raa_sum
+                row['포수 수비 RAA'] = away_catcher_raa_sum 
+
+            for j in range(11):
+                index = i*12 + 1 + j
+                row[columns[j+8]] = data[index]
+
+            df = pd.concat([df, pd.DataFrame([row])], ignore_index=True)
+    
+    return df
